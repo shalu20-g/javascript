f0e211f12e6537c4d7e415c010e1c92ac400b3ba
shalu <shaluprajapati0902@gmail.com>
2026-05-26 15:13:30 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,21 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3 + 3) // we are using nodejs, not browser
+
+console.log(3 + 3)  //code readability should be high,
+
+// number => 2 to power 53
+// bigint => for numbers bigger than 2 to power 53
+// string => "hello", 'hello', `hello`
+// boolean => true, false
+// null => standalone value meaning "nothing", "empty" or "value unknown"
+
+// undefined => value is not assigned
+// symbol => unique identifier
+
+//object => complex data structure that can store collections of data and more complex entities
+
+// typeof operator => to find the type of a variable
+
+console.log(typeof undefined); //undefined
+console.log(typeof null); //object
